08ea0d23b9ea483bbf87f0bbd48b71ff491c9ed5
Damilare Emmanuel <emmanuel@platformlead.com>
2019-04-17 15:02:45 +0100

    reverse string first approach

---
+++ reverse-string/index.js
@@ -0,0 +1,12 @@
+
+//**Problem**
+//Given a String, reverse the whole string.
+
+//**Usage**
+//reversedString('hello world') = 'dlrow olleh'
+
+//First approach to reverse a string user the built in javascript reserve helper method
+
+const reversedString = str =>{
+        return str.split('').reduce().join('');
+},
